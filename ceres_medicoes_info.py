770415579eb4e217ec41417e009830e9d103eb90
Fernando Possebon <possebon@gmail.com>
2024-05-27 16:11:01 -0300

    Fix some logic errors

--- ceres_medicoes_info.py
+++ ceres_medicoes_info.py
@@ -1,11 +1,10 @@
 from locust import HttpUser, TaskSet, task, between
 import random
 from faker import Faker
-import pymysql
 from sqlalchemy import create_engine, MetaData, Table, select, update
-from dotenv import load_dotenv
 from sqlalchemy.orm import sessionmaker
 from contextlib import contextmanager
+from dotenv import load_dotenv
 import os
 import logging
 
@@ -41,29 +40,29 @@ def get_session(engine):
     finally:
         session.close()
 
-def get_available_database():
+def get_available_databases(user_count):
     engine = create_engine(REPORT_DB_URL)
-    metadata = MetaData(bind=engine)
-    database_list = Table('database_list', metadata, autoload=True)
+    metadata = MetaData()
+    database_list = Table('database_list', metadata, autoload_with=engine)
     
     with engine.connect() as connection:
-        query = select([database_list.c.database_name]).where(database_list.c.status == 'available').order_by(database_list.c.database_name).limit(1)
-        result = connection.execute(query).fetchone()
+        query = select([database_list.c.database_name]).where(database_list.c.status == 'available').order_by(database_list.c.database_name).limit(user_count)
+        result = connection.execute(query).fetchall()
         if result:
-            db_name = result[0]
-            update_query = update(database_list).where(database_list.c.database_name == db_name).values(status='executing')
+            db_names = [row[0] for row in result]
+            update_query = update(database_list).where(database_list.c.database_name.in_(db_names)).values(status='executing')
             connection.execute(update_query)
-            return db_name
+            return db_names
         else:
-            logging.error('No available database found')
+            logging.error('No available databases found')
             return None
 
-def get_valid_ids(engine, table, id_field):
-    metadata = MetaData(bind=engine)
-    metadata.reflect(bind=engine)
-    table = Table(table, metadata, autoload=True)
-    session = sessionmaker(bind=engine)()
-    return [row[0] for row in session.query(getattr(table.c, id_field)).all()]
+def get_valid_ids(engine, table_name, id_field):
+    metadata = MetaData()
+    table = Table(table_name, metadata, autoload_with=engine)
+    with engine.connect() as conn:
+        result = conn.execute(select([table.c[id_field]]))
+        return [row[id_field] for row in result]
 
 def generate_random_data(valid_silo_ids, valid_filial_ids):
     return {
@@ -105,43 +104,48 @@ def generate_random_data(valid_silo_ids, valid_filial_ids):
         "Estacao_3_Anemometro_Velocidade_Vento": round(random.uniform(0, 10), 2),
         "Estacao_3_Anemometro_Direcao_Vento": random.randint(0, 359),
     }
+
 def insert_data(engine, db_name):
     valid_silo_ids = get_valid_ids(engine, 'ceres_silos', 'ID_Silo')
     valid_filial_ids = get_valid_ids(engine, 'ceres_filiais', 'ID_Filial')
-    metadata = MetaData(bind=engine)
-    metadata.reflect(bind=engine)
-    ceres_medicoes_info = Table('ceres_medicoes_info', metadata, autoload=True)
-    while True:
-        data = generate_random_data(valid_silo_ids, valid_filial_ids)
-        ins = ceres_medicoes_info.insert().values(data)
-        try:
-            with get_session(engine) as session:
-                session.execute(ins)
-                session.commit()
-            logging.info(f'Insert successful into {db_name}')
-        except Exception as e:
-            logging.error(f'Insert failed into {db_name}: {e}')
+    metadata = MetaData()
+    ceres_medicoes_info = Table('ceres_medicoes_info', metadata, autoload_with=engine)
+    
+    data = generate_random_data(valid_silo_ids, valid_filial_ids)
+    ins = ceres_medicoes_info.insert().values(data)
+    
+    try:
+        with get_session(engine) as session:
+            session.execute(ins)
+            session.commit()
+        logging.info(f'Insert successful into {db_name}')
+    except Exception as e:
+        logging.error(f'Insert failed into {db_name}: {e}')
 
 class DatabaseTaskSet(TaskSet):
     def on_start(self):
         self.engine_dict = {}
         num_databases = self.user.environment.runner.user_count
-        database_list = get_available_database()[num_databases]
+        database_list = get_available_databases(num_databases)
 
-        for db_name in database_list:
-            engine = create_engine(f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{db_name}")
-            self.engine_dict[db_name] = engine
+        if database_list:
+            for db_name in database_list:
+                engine = create_engine(f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{db_name}")
+                self.engine_dict[db_name] = engine
+        else:
+            logging.error('No available databases to use')
 
     @task(10)
     def perform_insertion(self):
-        db_name = random.choice(list(self.engine_dict.keys()))
-        engine = self.engine_dict[db_name]
-        insert_data(engine, db_name)
-
-
-
+        if self.engine_dict:
+            db_name = random.choice(list(self.engine_dict.keys()))
+            engine = self.engine_dict[db_name]
+            insert_data(engine, db_name)
 
 class DatabaseUser(HttpUser):
     tasks = [DatabaseTaskSet]
     wait_time = between(1, 5)
 
+if __name__ == "__main__":
+    import os
+    os.system("locust -f ceres_medicoes_info.py")
